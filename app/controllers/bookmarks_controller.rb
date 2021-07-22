1854f54ce7016327353477bf336c8c117addc17e
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-07-22 21:43:57 +0800

    Completed up to the formatting part on watch list exercise - still having issues on 2 rspec tests'

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+  before_action :find_list, only: [:create, :new]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = @list.id
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+end
